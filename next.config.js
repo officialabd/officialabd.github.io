eea6d971bcfa5b3ee0a8b77fb3cb5198f575ccd1
officialabd <officialabdib@gmail.com>
2023-08-02 17:22:11 +0400

    configure imgix and github pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+    // trim off `<owner>/`
+    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+    assetPrefix = `/${repo}/`
+    basePath = `/${repo}`
+}
+
+module.exports = {
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+    images: {
+        loader: 'imgix',
+        path: 'https://officialabdib-imgs.imgix.net/public/',
+    },
+}
