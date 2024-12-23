62c8e66fba271841ca0e6d42a2f6968d28aee071
officialabd <officialabdib@gmail.com>
2024-12-23 17:59:16 +0200

    removed dummy comments

--- next.config.js
+++ next.config.js
@@ -1,25 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-
-// const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-// let assetPrefix = ''
-// let basePath = '/'
-
-// if (isGithubActions) {
-//     // trim off `<owner>/`
-//     const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-//     assetPrefix = `/${repo}/`
-//     basePath = `/${repo}`
-// }
-
 module.exports = {
-    // assetPrefix: assetPrefix,
-    // basePath: basePath,
-    // images: {
-    //     loader: 'imgix',
-    //     path: 'https://officialabdib-imgs.imgix.net/public/',
-    // },
     output: 'export',
 }
